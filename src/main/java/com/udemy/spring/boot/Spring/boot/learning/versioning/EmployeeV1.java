fdc9812424133392a2b5d7d3da160ae74b4ea3f1
Sachin <sachinkmr375@gmail.com>
2021-02-11 23:18:44 +0530

    Versioning

---
+++ src/main/java/com/udemy/spring/boot/Spring/boot/learning/versioning/EmployeeV1.java
@@ -0,0 +1,14 @@
+package com.udemy.spring.boot.Spring.boot.learning.versioning;
+
+public class EmployeeV1 {
+
+    private final String name;
+
+    public EmployeeV1(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
